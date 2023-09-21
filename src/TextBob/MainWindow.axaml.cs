9c870f644907a139184f7976ace1d5529d6eda75
Premysl Fara <premysl.fara@commerzbank.com>
2023-09-21 15:59:18 +0200

    Cursor position base.

--- src/TextBob/MainWindow.axaml.cs
+++ src/TextBob/MainWindow.axaml.cs
@@ -10,4 +10,51 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+
+
+    private void ClearButtonClicked(object sender, RoutedEventArgs e)
+    {
+        mainTextBox.Clear();
+        mainTextBox.Focus();
+
+        UpdateInfoText();
+    }
+
+
+    private void MainTextBoxGotFocus(object sender, GotFocusEventArgs e)
+    {
+        UpdateInfoText();
+    }
+
+
+    private void MainTextBoxKeyUp(object sender, KeyEventArgs e)
+    {
+        UpdateInfoText();
+    }
+
+
+    private void MainTextBoxPointerMoved(object sender, PointerEventArgs e)
+    {
+        UpdateInfoText();
+    }
+
+
+    private void UpdateInfoText()
+    {
+        if (mainTextBox.Text == null)
+        {
+            return;
+        }
+
+        // https://stackoverflow.com/questions/15577464/how-to-count-of-sub-string-occurrences-within-a-string-not-char-occurrences
+        var pos = 0;
+        var linesCount = 0;
+        while ((pos < mainTextBox.CaretIndex) && (pos = mainTextBox.Text.IndexOf(mainTextBox.NewLine, pos)) != -1)
+        {
+            linesCount++;
+            pos += mainTextBox.NewLine.Length;
+        }
+
+        infoTextBlock.Text = $"Length: {mainTextBox.Text.Length}, position: {mainTextBox.CaretIndex}, line: {linesCount}";
+    }
 }
