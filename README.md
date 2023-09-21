c35d27d64bed79c40bd3ed127d5543519db02caf
Premysl Fara <premysl.fara@commerzbank.com>
2023-09-21 08:52:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Text Bob
+
+A simple text editor.
---
+++ src/TextBob/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+
+namespace TextBob;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
