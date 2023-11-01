ba36b6fe5fe00d5702b5e06076bd187d99d0d753
Premysl Fara <premysl.fara@gmail.com>
2023-11-01 08:32:57 +0100

    MainWindowViewModel contains reference to AppViewModel.

--- src/TextBob/ViewModels/MainWindowViewModel.cs
+++ src/TextBob/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,32 @@ namespace TextBob.ViewModels;
 
 internal class MainWindowViewModel : ViewModelBase
 {
+    private AppViewModel? _appViewModel;
+
+    /// <summary>
+    /// The app view model.
+    /// </summary>
+    public AppViewModel? AppViewModel
+    {
+        get
+        {
+            return _appViewModel;
+        }
+
+        set
+        {
+            if (_appViewModel == value)
+            {
+                return;
+            }
+
+            _appViewModel = value;
+            
+            RaisePropertyChanged();
+        }
+    }
+
+
     private string? _title;
 
     /// <summary>
