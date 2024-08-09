9ef2fa194fa9475c29b7e66e248c22ce713497fe
Premysl Fara <premysl.fara@gmail.com>
2024-08-09 10:42:27 +0200

    SnapshotFile class overrides ToString().

--- src/TextBob/Models/Settings.cs
+++ src/TextBob/Models/Settings.cs
@@ -126,4 +126,12 @@ public class SnapshotFile
     /// Path to the snapshot file.
     /// </summary>
     public string Path { get; set; } = Defaults.DefaultSnapshotFilePath;
+
+    
+    /// <summary>
+    /// Returns the name of the snapshot.
+    /// </summary>
+    /// <returns>The name of the snapshot.</returns>
+    public override string ToString()
+        => Name;
 }
