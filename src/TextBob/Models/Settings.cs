7a5fa893aa0d108a75c37417db9015dc5c2f2da3
enif77 <premysl.fara@gmail.com>
2024-09-06 14:51:07 +0200

    SnapshotFile class moved to a separated file.

--- src/TextBob/Models/Settings.cs
+++ src/TextBob/Models/Settings.cs
@@ -110,28 +110,3 @@ public class Settings
         public Settings? Settings { get; set; }
     }
 }
-
-
-/// <summary>
-/// Describes a snapshot file.
-/// </summary>
-public class SnapshotFile
-{
-    /// <summary>
-    /// Name of the snapshot displayed in the UI.
-    /// </summary>
-    public string Name { get; set; } = Defaults.DefaultSnapshotName;
-    
-    /// <summary>
-    /// Path to the snapshot file.
-    /// </summary>
-    public string Path { get; set; } = Defaults.DefaultSnapshotFilePath;
-
-    
-    /// <summary>
-    /// Returns the name of the snapshot.
-    /// </summary>
-    /// <returns>The name of the snapshot.</returns>
-    public override string ToString()
-        => Name;
-}
